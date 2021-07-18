679636d26a53a1478d24e71dee9e5e814db28dec
Rui Braga <rui.braga@hitachivantara.com>
2021-07-19 00:10:22 +0100

    Commit

--- ScriptsRunner/Scripts/Rename this RUN Publish Job.sql
+++ ScriptsRunner/Scripts/Rename this RUN Publish Job.sql
@@ -8,20 +8,19 @@ EXEC msdb.dbo.sp_start_job @job_name;
 
 WHILE @execution_status not in (4,5)
 BEGIN
-	IF OBJECT_ID('sp_get_SQL_job_execution_status', 'P') IS NOT NULL
-BEGIN
-    WAITFOR DELAY '00:00:03.000';
-EXEC	[dbo].[sp_get_SQL_job_execution_status]
-            @job_name = @job_name,
-            @execution_status = @execution_status OUTPUT
-END
-ELSE
-BEGIN
     WAITFOR DELAY '$WaitForDelay$';
-    BREAK
-END
+	IF OBJECT_ID('sp_get_SQL_job_execution_status', 'P') IS NOT NULL
+        BEGIN
+            EXEC	[dbo].[sp_get_SQL_job_execution_status]
+                    @job_name = @job_name,
+                    @execution_status = @execution_status OUTPUT
+        END
+    ELSE
+        BEGIN
+             BREAK
+        END
 END
     IF  (@execution_status=4)
         BEGIN
-        select 'Job '''+ @job_name +''' finished successfully'
+            select 'Job '''+ @job_name +''' finished successfully'
         END
