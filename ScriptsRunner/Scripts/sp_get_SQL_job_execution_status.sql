5b730e29aa310de7885e8c70a2b567a95350a228
Rui Braga <rui.braga@hitachivantara.com>
2021-07-19 00:46:20 +0100

    [dbo].[sp_get_SQL_job_execution_status]

--- ScriptsRunner/Scripts/sp_get_SQL_job_execution_status.sql
+++ ScriptsRunner/Scripts/sp_get_SQL_job_execution_status.sql
@@ -1,65 +1,65 @@
-create procedure sp_get_SQL_job_execution_status
-	(
-		@job_name sysname
-		, @select_data int =0
-		, @execution_status int =null output
-	
-	)
-	as
+create procedure [dbo].[sp_get_SQL_job_execution_status]
+(
+    @job_name sysname
+, @select_data int =0
+, @execution_status int =null output
 
-	set nocount on
+)
+as
 
-
-	/*
-		Is the execution status for the jobs. 
-		Value Description 
-		0 Returns only those jobs that are not idle or suspended.  
-		1 Executing. 
-		2 Waiting for thread. 
-		3 Between retries. 
-		4 Idle. 
-		5 Suspended. 
-		7 Performing completion actions 
-
-	*/
-	
-	declare	@job_id UNIQUEIDENTIFIER 
-		, @is_sysadmin INT
-		, @job_owner   sysname
-
-	select @job_id = job_id from msdb..sysjobs_view where name = @job_name 
-	select @is_sysadmin = ISNULL(IS_SRVROLEMEMBER(N'sysadmin'), 0)
-	select @job_owner = SUSER_SNAME()
-
-	CREATE TABLE #xp_results (job_id                UNIQUEIDENTIFIER NOT NULL,
-	                            last_run_date         INT              NOT NULL,
-	                            last_run_time         INT              NOT NULL,
-	                            next_run_date         INT              NOT NULL,
-	                            next_run_time         INT              NOT NULL,
-	                            next_run_schedule_id  INT              NOT NULL,
-	                            requested_to_run      INT              NOT NULL, -- BOOL
-	                            request_source        INT              NOT NULL,
-	                            request_source_id     sysname          COLLATE database_default NULL,
-	                            running               INT              NOT NULL, -- BOOL
-	                            current_step          INT              NOT NULL,
-	                            current_retry_attempt INT              NOT NULL,
-	                            job_state             INT              NOT NULL)
+    set nocount on
 
 
-	IF ((@@microsoftversion / 0x01000000) >= 8) -- SQL Server 8.0 or greater
-		    INSERT INTO #xp_results
-		    EXECUTE master.dbo.xp_sqlagent_enum_jobs @is_sysadmin, @job_owner, @job_id
-  	ELSE
-		    INSERT INTO #xp_results
-		    EXECUTE master.dbo.xp_sqlagent_enum_jobs @is_sysadmin, @job_owner
+    /*
+        Is the execution status for the jobs. 
+        Value Description 
+        0 Returns only those jobs that are not idle or suspended.  
+        1 Executing. 
+        2 Waiting for thread. 
+        3 Between retries. 
+        4 Idle. 
+        5 Suspended. 
+        7 Performing completion actions 
+
+    */
+
+declare	@job_id UNIQUEIDENTIFIER
+, @is_sysadmin INT
+, @job_owner   sysname
+
+select @job_id = job_id from msdb..sysjobs_view where name = @job_name
+select @is_sysadmin = ISNULL(IS_SRVROLEMEMBER(N'sysadmin'), 0)
+select @job_owner = SUSER_SNAME()
+
+DECLARE  @xp_results TABLE (job_id                UNIQUEIDENTIFIER NOT NULL,
+                            last_run_date         INT              NOT NULL,
+                            last_run_time         INT              NOT NULL,
+                            next_run_date         INT              NOT NULL,
+                            next_run_time         INT              NOT NULL,
+                            next_run_schedule_id  INT              NOT NULL,
+                            requested_to_run      INT              NOT NULL, -- BOOL
+                            request_source        INT              NOT NULL,
+                            request_source_id     sysname          COLLATE database_default NULL,
+                            running               INT              NOT NULL, -- BOOL
+                            current_step          INT              NOT NULL,
+                            current_retry_attempt INT              NOT NULL,
+                            job_state             INT              NOT NULL)
 
 
-	--declare @execution_status int
-	set @execution_status = (select job_state from #xp_results)
+    IF ((@@microsoftversion / 0x01000000) >= 8) -- SQL Server 8.0 or greater
+        INSERT INTO @xp_results
+            EXECUTE master.dbo.xp_sqlagent_enum_jobs @is_sysadmin, @job_owner, @job_id
+    ELSE
+        INSERT INTO @xp_results
+            EXECUTE master.dbo.xp_sqlagent_enum_jobs @is_sysadmin, @job_owner
 
-	drop table #xp_results
 
-	if @select_data =1 
-		select @job_name as 'job_name', @execution_status as 'execution_status'
+    --declare @execution_status int
+    set @execution_status = (select job_state from @xp_results)
 
-	set nocount off
+    if @select_data =1
+        select @job_name as 'job_name', @execution_status as 'execution_status'
+
+    set nocount off
+GO
+
